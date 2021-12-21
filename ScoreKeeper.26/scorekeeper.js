4345fd384add0568271a54a680be0a6a76285649
msudawg07 <93173601+msudawg07@users.noreply.github.com>
2021-12-21 11:49:20 -0600

    Update scorekeeper.js

--- ScoreKeeper.26/scorekeeper.js
+++ ScoreKeeper.26/scorekeeper.js
@@ -1,1 +1,33 @@
+let pOne = 0;
+let pTwo = 0;
+let playingTo = document.querySelector('.playingTo')
+let button = document.querySelectorAll('.buttonsMain');
+for (let click of button) {
+    click.addEventListener('click', () => {
+        if (click.innerText == '1+PlayerOne') {
+            pOne += 1;
+        } else if (click.innerText == '1+PlayerTwo') {
+            pTwo += 1;
+        }
+        document.querySelector('.score').innerText = `${pOne} to ${pTwo}`;
+        if (pOne == playingTo.value || pTwo == playingTo.value) {
+            disableButtons();
+        }
+    }
+    )
+}
+
+
+function disableButtons() {
+    button.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+
+
+let reset = document.querySelector('.reset');
+reset.addEventListener('click', () => {
+    window.location.reload();
+})
 
